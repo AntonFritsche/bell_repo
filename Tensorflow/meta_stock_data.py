1b7a8df14b9d311e8eda6911c4e77d3247e6ccfb
AntonFritsche <antonfritschedresden@gmail.com>
2024-10-24 22:50:34 +0200

    a few changes

--- Tensorflow/meta_stock_data.py
+++ Tensorflow/meta_stock_data.py
@@ -3,7 +3,9 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-
-# Download latest version
 path = kagglehub.dataset_download("zongaobian/meta-stock-data-and-key-affiliated-companies")
 print("Path to dataset files:", path)
+
+data = pd.read_csv(path)
+
+# print(data.head)
