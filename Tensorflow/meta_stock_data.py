419e7b0c07fc1d6fcc6da29418f5c35eac1cb95a
AntonFritsche <antonfritschedresden@gmail.com>
2024-10-25 22:56:27 +0200

    little changes

--- Tensorflow/meta_stock_data.py
+++ Tensorflow/meta_stock_data.py
@@ -3,9 +3,17 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-path = kagglehub.dataset_download("zongaobian/meta-stock-data-and-key-affiliated-companies")
+path = kagglehub.dataset_download(r"zongaobian/meta-stock-data-and-key-affiliated-companies")
 print("Path to dataset files:", path)
 
-data = pd.read_csv(path)
+def convert_path(pa):
+    for i in pa:
+        if i == "\\":
+            pa[pa.index(i)] = "/"
+        else:
+            continue
+    return pa
+new_path = convert_path(path) + "META_daily_data.csv"
+data = pd.read_csv(new_path)
 
-# print(data.head)
+print(data.head)
