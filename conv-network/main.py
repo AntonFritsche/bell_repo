47927f6d1796ef45a746712b4176556bc658f0a6
AntonFritsche <antonfritschedresden@gmail.com>
2024-12-13 07:52:58 +0100

    split up class/model and main code

--- conv-network/main.py
+++ conv-network/main.py
@@ -2,14 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import pandas as pd
-from main import ConvModel
+import model
 
 # instantate the convolution model
-model = ConvModel()
+conv_model = model.ConvModel()
 print(model)
 
 # list of parameters
-params = list(model.parameters())
+params = list(conv_model.parameters())
 print("length parameters: ", len(params))
 print("output_size: ", params[0].size())
 
