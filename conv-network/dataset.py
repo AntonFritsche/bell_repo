16a3b3cb4b57618094ca12e2211b1bc16717f606
AntonFritsche <antonfritschedresden@gmail.com>
2024-12-17 14:14:09 +0100

    resized all train and test images with func from dataset.py "resize_images"

--- conv-network/dataset.py
+++ conv-network/dataset.py
@@ -3,6 +3,8 @@ import shutil
 import random
 from PIL import Image
 import csv
+import re
+import cv2
 
 def create_datasets(source_folder, dest_folder, test_size=200):
     train_folder = os.path.join(dest_folder, 'train')
@@ -54,7 +56,7 @@ def convert_images_to_grayscale(folder_path):
             print(f"Fehler beim Konvertieren von {image_path}: {e}")
 
 greyscale_folder = "./test"
-convert_images_to_grayscale(greyscale_folder)
+# convert_images_to_grayscale(greyscale_folder)
 
 def create_csv_from_dataset(folder_path, csv_path):
     with open(csv_path, mode='w', newline='') as csv_file:
@@ -72,5 +74,28 @@ def create_csv_from_dataset(folder_path, csv_path):
 
 train_csv_path = os.path.join(conv_network_folder, 'train.csv')
 test_csv_path = os.path.join(conv_network_folder, 'test.csv')
-create_csv_from_dataset(os.path.join(conv_network_folder, 'train'), train_csv_path)
-create_csv_from_dataset(os.path.join(conv_network_folder, 'test'), test_csv_path)
+# create_csv_from_dataset(os.path.join(conv_network_folder, 'train'), train_csv_path)
+# create_csv_from_dataset(os.path.join(conv_network_folder, 'test'), test_csv_path)
+
+def resize_images(folder_path, size=(500, 500)):
+    images = [os.path.join(folder_path, img) for img in os.listdir(folder_path) 
+              if os.path.isfile(os.path.join(folder_path, img))]
+
+    for image_path in images:
+        try:
+            with Image.open(image_path) as img:
+                filename = os.path.basename(image_path)
+                cleared_filename = re.sub(r'[^a-zA-Z0-9]', '', filename.split('.')[0])  # Unerwünschte Zeichen entfernen
+                
+                resized_image = img.resize(size)
+                
+                save_path = os.path.join(folder_path, f"{cleared_filename}.jpg")
+                resized_image.save(save_path)
+                print(f"Bild resized und gespeichert: {save_path}")
+        except Exception as e:
+            print(f"Fehler beim Resizen von {image_path}: {e}")
+
+train_folder = "./train"
+test_folder = "./test"
+resize_images_in_place(train_folder)
+resize_images_in_place(test_folder)
