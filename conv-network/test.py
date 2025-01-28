da5dea1a2ebc72af18b0373a50f3d556ced42a80
AntonFritsche <antonfritschedresden@gmail.com>
2025-01-28 17:16:41 +0100

    Testing for cuda support

--- conv-network/test.py
+++ conv-network/test.py
@@ -1,3 +1,8 @@
 import torch
 
+print(f"cuda 0 : {torch.device('cuda:0')}")
+print(f"cuda 1 : {torch.device('cuda:1')}")
+print(f"cuda 2 : {torch.device('cuda:2')}")
+
 print(torch.cuda.is_available())
+print(torch.version.hip)
