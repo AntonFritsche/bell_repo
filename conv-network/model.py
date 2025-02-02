1b02705fee1cf7b1c363d19081e44718d17ae1f0
AntonFritsche <antonfritschedresden@gmail.com>
2025-02-02 13:55:34 +0100

    Added parameters for layers in model.py

--- conv-network/model.py
+++ conv-network/model.py
@@ -7,39 +7,84 @@ import torch.nn as nn
 # import cv2
 
 class ConvModel(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self,
+                 layer_1_in_channels_param: int,
+                 layer_1_out_channels_param: int,
+                 layer_2_in_channels_param: int,
+                 layer_2_out_channels_param: int,
+                 layer_3_in_channels_param: int,
+                 layer_3_out_channels_param: int,
+                 layer_4_in_channels_param: int,
+                 layer_4_out_channels_param: int,
+                 layer_5_in_channels_param: int,
+                 layer_5_out_channels_param: int,
+                 layer_6_in_channels_param: int,
+                 layer_6_out_channels_param: int,
+                 linear_layer_1_in_features_param: int,
+                 linear_layer_1_out_features_param: int,
+                 linear_layer_2_features_param: int,
+                 linear_layer_2_out_features_param: int,
+            ) -> None:
+
         super(ConvModel, self).__init__()
         self.filter_size = 3 # -> 6 conv layers
-        
+
+        # convolution layer 1 parameters
+        self.layer_1_in_channels = layer_1_in_channels_param
+        self.layer_1_out_channels = layer_1_out_channels_param
+
+        # convolution layer 2 parameters
+        self.layer_2_in_channels = layer_2_in_channels_param
+        self.layer_2_out_channels = layer_2_out_channels_param
+
+        # convolution layer 3 parameters
+        self.layer_3_in_channels = layer_3_in_channels_param
+        self.layer_3_out_channels = layer_3_out_channels_param
+
+        # convolution layer 4 parameters
+        self.layer_4_in_channels = layer_4_in_channels_param
+        self.layer_4_out_channels = layer_4_out_channels_param
+
+        # convolution layer 5 parameters
+        self.layer_5_in_channels = layer_5_in_channels_param
+        self.layer_5_out_channels = layer_5_out_channels_param
+
+        # convolution layer 6 parameters
+        self.layer_6_in_channels = layer_6_in_channels_param
+        self.layer_6_out_channels = layer_6_out_channels_param
+
+        # linear layer 1 parameters
+        self.linear_layer_1_in_features = linear_layer_1_in_features_param
+        self.linear_layer_1_out_features = linear_layer_1_out_features_param
+
+        # linear layer 2 parameters
+        self.linear_layer_2_in_features = linear_layer_2_features_param
+        self.linear_layer_2_out_features = linear_layer_2_out_features_param
+
         # convolution 1
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=4, kernel_size=self.filter_size)
+        self.conv1 = nn.Conv2d(in_channels=self.layer_1_in_channels, out_channels=self.layer_1_out_channels, kernel_size=self.filter_size)
 
         # convolution 2
-        self.conv2 = nn.Conv2d(in_channels=4, out_channels=8, kernel_size=self.filter_size)
+        self.conv2 = nn.Conv2d(in_channels=self.layer_2_in_channels, out_channels=self.layer_2_out_channels, kernel_size=self.filter_size)
 
         # convolution 3
-        self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=self.filter_size)
+        self.conv3 = nn.Conv2d(in_channels=self.layer_3_in_channels, out_channels=self.layer_3_out_channels, kernel_size=self.filter_size)
 
         # convolution 4
-        self.conv4 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=self.filter_size)
+        self.conv4 = nn.Conv2d(in_channels=self.layer_4_in_channels, out_channels=self.layer_4_out_channels, kernel_size=self.filter_size)
 
         # convolution 5
-        self.conv5 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=self.filter_size)
+        self.conv5 = nn.Conv2d(in_channels=self.layer_5_in_channels, out_channels=self.layer_5_out_channels, kernel_size=self.filter_size)
 
         # convolution 6
-        self.conv6 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=self.filter_size)
+        self.conv6 = nn.Conv2d(in_channels=self.layer_6_in_channels, out_channels=self.layer_6_out_channels, kernel_size=self.filter_size)
         
         # fully connected layer 1
-        self.fc1 = nn.Linear(in_features=128, out_features=64)
+        self.fc1 = nn.Linear(in_features=self.linear_layer_1_in_features, out_features=self.linear_layer_1_out_features)
         
         # fully connected layer 2
-        self.fc2 = nn.Linear(in_features=64, out_features=32)
+        self.fc2 = nn.Linear(in_features=self.linear_layer_2_in_features, out_features=self.linear_layer_2_out_features)
 
-        # fully connected layer 3
-        self.fc3 = nn.Linear(in_features=32, out_features=8)
-
-        # fully connected layer 4
-        self.fc4 = nn.Linear(in_features=8, out_features=2)
 
     # noinspection PyPep8Naming
     def forward(self, x):
